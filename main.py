fb77ed04623255f8c6c8359c0c4a4e22320ac065
maksym <01178511@pw.edu.pl>
2023-12-28 13:26:54 +0100

    argparse for main

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ from MnistNN import MnistNeuralNetwork
 from NeuralNetwork import NeuralNetwork
 from constants import *
 from util import *
+from argparse import ArgumentParser
 
 
 def main():
+    parser = ArgumentParser(description="Initialize the network with parameters")
+
+    parser.add_argument('-H', '--HiddenLayerSizes', nargs='+', type=int,
+                        help='The size of the hidden layers in the network')
+    parser.add_argument('-E', '--Epochs', type=int, help='The number of epochs for the model to train over')
+    parser.add_argument('-L', '--LearningRate', type=float, help='The learning rate of the model')
+
+    args = parser.parse_args()
+
     (train_x, train_y), (test_x, test_y) = import_data()
-    net = MnistNeuralNetwork(layer_sizes=MNIST_NN_LAYERS, activation_function=NeuralNetwork.sigmoid, epochs=EPOCHS,
-                             learning_rate=LEARNING_RATE)
+    net = MnistNeuralNetwork(layer_sizes=[784] + args.HiddenLayerSizes + [10],
+                             epochs=args.Epochs, learning_rate=args.LearningRate)
     net.fit(train_x, train_y)
     classification = net.classify(test_x)
     display_confusion_matrix(classification, test_y)
